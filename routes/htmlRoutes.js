4b2d410343b2831b5699fd7dffbd8c97d1375477
Neil Froggatt <neilfr@live.com>
2019-02-04 14:22:09 -0500

    removed duplicate routes from htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,40 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
-  /* app.get("/teamManager", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-*/
   // Load teamManager page
   app.get("/teamManager", function(req, res) {
     db.Teams.findAll({}).then(function(dbTeams) {
       res.render("teamManager", {
         msg: "Welcome!",
         teams: dbTeams
-      });
-    });
-  });
-
-  // Load teamManager page and pass in a team by id
-  app.get("/team/:id", function(req, res) {
-    db.Teams.findOne({ where: { id: req.params.id } }).then(function(dbTeam) {
-      res.render("teamManager", {
-        team: dbTeam
       });
     });
   });
@@ -49,17 +21,6 @@ module.exports = function(app) {
     });
   });
 
-  // Load playerManager page and pass in a player by id
-  app.get("/player/:id", function(req, res) {
-    db.Players.findOne({ where: { id: req.params.id } }).then(function(
-      dbPlayer
-    ) {
-      res.render("playerManager", {
-        player: dbPlayer
-      });
-    });
-  });
-
   // Load gameManager page
   app.get("/gameManager", function(req, res) {
     db.Games.findAll({}).then(function(dbGames) {
@@ -69,17 +30,6 @@ module.exports = function(app) {
       });
     });
   });
-
-  // Load gameManager page and pass in a game by id
-  /*
-  app.get("/game/:id", function(req, res) {
-    db.Games.findOne({ where: { id: req.params.id } }).then(function(dbGame) {
-      res.render("gameManager", {
-        game: dbGame
-      });
-    });
-  });
-*/
 
   // Load gamePicker page
   app.get("/gamePicker", function(req, res) {
