92ad53ace40f0880ce19f52dfebca4b34bc1ba3c
Neil Froggatt <neilfr@live.com>
2019-02-07 22:45:54 -0500

    goal data pushed to scoretracker.html for home team

--- public/js/scoretracker.js
+++ public/js/scoretracker.js
@@ -3,6 +3,14 @@ var socket = io();
 socket.on("myEvent", function(msg) {
   console.log("and the object is:");
   console.log(msg);
-  var x = document.getElementById("message");
-  x.innerHTML = msg;
+
+  var game = document.getElementById("game");
+  var player = document.getElementById("player");
+  var team = document.getElementById("team");
+  var timeRemaining = document.getElementById("timeRemaining");
+
+  game.innerHTML = msg.gameID;
+  player.innerHTML = msg.playerID;
+  team.innerHTML = msg.teamID;
+  timeRemaining.innerHTML = msg.timeRemaining;
 });
