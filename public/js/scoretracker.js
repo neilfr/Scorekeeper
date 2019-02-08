ce0a8630639f48643ca892a9331933b1c1e04528
Neil Froggatt <neilfr@live.com>
2019-02-07 23:51:21 -0500

    scoretracker displays all goals in a log

--- public/js/scoretracker.js
+++ public/js/scoretracker.js
@@ -1,7 +1,7 @@
 //scoretracker.js
 var socket = io();
 socket.on("myEvent", function(msg) {
-  var game = document.getElementById("game");
+  /*  var game = document.getElementById("game");
   var player = document.getElementById("player");
   var team = document.getElementById("team");
   var timeRemaining = document.getElementById("timeRemaining");
@@ -9,5 +9,12 @@ socket.on("myEvent", function(msg) {
   game.innerHTML = msg.gameID;
   player.innerHTML = msg.playerID;
   team.innerHTML = msg.teamID;
-  timeRemaining.innerHTML = msg.timeRemaining;
+  timeRemaining.innerHTML = msg.timeRemaining;*/
+  var $goalTable = $("#goalTable");
+  var $tr = $("<tr>");
+  $tr.append("<td>" + msg.gameID + "</td>");
+  $tr.append("<td>" + msg.playerID + "</td>");
+  $tr.append("<td>" + msg.teamID + "</td>");
+  $tr.append("<td>" + msg.timeRemaining + "</td>");
+  $goalTable.append($tr);
 });
