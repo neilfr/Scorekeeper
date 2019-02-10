fc80137f98e90b5ed38d55c0cd79b71d7861ca48
Neil Froggatt <neilfr@live.com>
2019-02-10 16:59:22 -0500

    fixed missing bracket in htmlroutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,19 +20,17 @@ var path = require("path");
   });
 */
 
-module.exports = function (app) {
-  app.get("/", function (req, res) {
+module.exports = function(app) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  app.get("/home", function (req, res) {
+  app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/gameselect", function (req, res) {
+  app.get("/gameselect", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/gameselect.html"));
   });
-
-
 
   app.get("/scorepage", function(req, res) {
     db.Games.findAll({}).then(function(dbGames) {
@@ -46,12 +44,12 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/scoreboard.html"));
   });
 
-  app.get("/manage", function (req, res) {
+  app.get("/manage", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/managerpage.html"));
+  });
 
   app.get("/scoretracker", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/scoretracker.html"));
-
   });
 
   app.get("/index", function(req, res) {
@@ -92,7 +90,6 @@ module.exports = function (app) {
     });
   });
 
-
   // Load gameManager page
   app.get("/gameManager", function(req, res) {
     db.Game.findAll({}).then(function(dbGames) {
@@ -129,7 +126,7 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/gametracker", function (req, res) {
+  app.get("/gametracker", function(req, res) {
     res.sendFile(path.join(__dirname, "../gametracker.html"));
   });
 
