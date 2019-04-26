bce23513116a427047eaa51455a6faec4763065b
Neil Froggatt <neilfr@live.com>
2019-04-26 09:56:00 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,15 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{c
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "scorekeeper"
-});
+  });
 }
 
 // Export connection for our ORM to use.
